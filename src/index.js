9f5b421f0b0209110f63d42dc4802e141c6bf321
Joachim Rosenberger <joachim.h.rosenberger@gmail.com>
2021-10-28 08:43:31 +0200

    included lodash in index.js and it is workinggit add -A!

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import { bro } from './bro'
 import "./styles/main.scss";
+import _ from 'lodash';
 
 
 console.log('Hello')
@@ -7,3 +8,6 @@ console.log('Joachim')
 let x = 10
 console.log(x)
 console.log(bro('Dude'))
+
+let a = _.chunk(['a', 'b', 'c', 'd'], 2);
+console.log(a)
